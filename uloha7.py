69053aaa7a688c0c406e59ad301e79bd2a85ff4b
Michal <polonecmichal36@gmail.com>
2025-09-30 16:09:15 +0200

    uloha7.py

---
+++ uloha7.py
@@ -0,0 +1,9 @@
+a= int(input("hodnota a: "))
+b= int(input("hodnota b: "))
+
+if a > b:
+    print("a je vacsisie nez b")
+if a < b:
+    print("a je mensie nez b")
+if a == b:
+    print("a je rovne b")

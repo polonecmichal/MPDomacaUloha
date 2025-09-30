557e5ec664e8ef3e6fcb541777d291e2a6449850
Michal <polonecmichal36@gmail.com>
2025-09-30 16:06:23 +0200

    uloha3.py

---
+++ uloha3.py
@@ -0,0 +1,7 @@
+a= int(input("spodna hodnota intervalu:"))
+b= int(input("vrchná hodnota intervalu:"))
+x= int(input("hodnota x :"))
+if a<x and x<b :
+    print("x nie je v intervale")  
+else:
+    print("x je v intervale")
